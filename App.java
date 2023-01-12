a2b05dce263a78e9f046e9cf954c636b366c0ad4
Angel <iangeleb19@gmail.com>
2023-01-12 12:01:51 +0100

    Proyecto Modificado

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Angel Jesus");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("12/01/2023");
+    System.out.println("La Bañeza");
   }
   
 }
